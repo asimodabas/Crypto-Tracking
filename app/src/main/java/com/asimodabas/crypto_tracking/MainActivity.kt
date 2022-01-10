0fdca7aaddd53773e52a22f5b8b3290cea18c597
Hacı Asım Odabaş <71982171+asimodabas@users.noreply.github.com>
2022-01-10 19:46:38 +0300

    API Key saved

--- app/src/main/java/com/asimodabas/crypto_tracking/MainActivity.kt
+++ app/src/main/java/com/asimodabas/crypto_tracking/MainActivity.kt
@@ -7,5 +7,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        //API Key : 2187154b76945f2373394aa34f7dc98a
+        //https://api.nomics.com/v1/prices?key=2187154b76945f2373394aa34f7dc98a
+
+
     }
 }
